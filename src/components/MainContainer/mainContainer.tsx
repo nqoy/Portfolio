57f95ab28bfa22dfd5b5f9a95fcb0dc925752272
nqoy <nqoy132@gmail.com>
2025-01-23 23:44:20 +0200

    Added profile div

--- src/components/MainContainer/mainContainer.tsx
+++ src/components/MainContainer/mainContainer.tsx
@@ -7,10 +7,17 @@ type MainContainerProps = {
 export const MainContainer = ({ children }: MainContainerProps) => {
   return (
     <main className={styles.mainContainer}>
+      <div className="profile">
+        <div className="profile-text">
+        <span>Computer Science & 3 years experiance in fullstack dev</span>
+        </div>
+      
       <CircularImage
         src="https://avatars.githubusercontent.com/u/93088356?v=4"
         alt="Failed loading image"
       />
+      </div>
+      
       {children}
     </main>
   );
